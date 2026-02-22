17d47533064442aef2e098ba60a7fe5ae88908a9
fouadmadi <fouad.madi@hotmail.com>
2026-02-22 11:44:52 -0800

    Initial Commit

---
+++ simpletranscribe/simpletranscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpletranscribeApp.swift
+//  simpletranscribe
+//
+//  Created by user on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct simpletranscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
